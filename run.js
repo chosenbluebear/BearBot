2ac023c507654e40caa20a2d297db517edf1d6d8
chosenbluebear <32991948+chosenbluebear@users.noreply.github.com>
2017-10-21 21:49:36 -0500

    Create run.js

---
+++ run.js
@@ -0,0 +1,8 @@
+{
+  "id": "308355412930068480",
+  "name": "OAuth2 Test",
+  "description": "",
+  "icon": null,
+  "secret": "MzA4MzU1NDEyOTMwMDY4NDgw.DM2WlQ.h3maJ1eEF71FJ3ocSKdJbkIxNVI",
+  "redirect_uris": ["https://discordapp.com/api/oauth2/authorize"],
+}
