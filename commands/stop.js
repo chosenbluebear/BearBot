2fb20a89ba9c7f3c76558a254116f72e03f76a5f
chosenbluebear <32991948+chosenbluebear@users.noreply.github.com>
2017-10-21 21:00:37 -0500

    Update stop.js

--- commands/stop.js
+++ commands/stop.js
@@ -3,20 +3,20 @@ const state = require('../utils/state');
 
 class StopCommand extends Command {
 	constructor() {
-		super('stop');
+		super('stop');'b!stop'
 	}
 
 	run(message) {
 		if(state.playing) {
-			message.reply('stopping, finally I thought you will never let me go');
+			message.reply('stopping, finally I thought you will never let me go'); Why stop?
 			state.songs = [];
 			state.stream.end();
 			state.streamDispatcher.end();
 			state.playing = false;
 		} else {
-			message.reply('there is currently no active song, why bother me?');
+			message.reply('there is currently no active song, why bother me?'); Wasting my time >:/
 		}
 	}
 }
 
-module.exports = StopCommand;
+module.exports = StopCommand;'b!stop'
