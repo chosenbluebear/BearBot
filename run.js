958229fb6f72118ebe13b429af907eb3610d0852
chosenbluebear <32991948+chosenbluebear@users.noreply.github.com>
2017-10-21 21:53:04 -0500

    Update run.js

--- run.js
+++ run.js
@@ -6,3 +6,6 @@
   "secret": "MzA4MzU1NDEyOTMwMDY4NDgw.DM2WlQ.h3maJ1eEF71FJ3ocSKdJbkIxNVI",
   "redirect_uris": ["https://discordapp.com/api/oauth2/authorize"],
 }
+{
+  OAUTH2_CLIENT_ID= 308355412930068480 OAUTH2_CLIENT_SECRET='MzA4MzU1NDEyOTMwMDY4NDgw.DM2WlQ.h3maJ1eEF71FJ3ocSKdJbkIxNVI' python app.py
+}
