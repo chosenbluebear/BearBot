f981a258578cc2da99f22f8ad437bbf0c10bcb7e
chosenbluebear <32991948+chosenbluebear@users.noreply.github.com>
2017-10-21 20:57:52 -0500

    Update queue_playlist.js

--- commands/queue_playlist.js
+++ commands/queue_playlist.js
@@ -6,15 +6,15 @@ const { getPlaylistSongs } = require('../utils/youtube');
 
 class QueuePlaylistCommand extends Command {
 	constructor() {
-		super('queue-playlist');
+		super('queue-playlist'); 'b!np'
 	}
 
 	run(message) {
-		const voiceChannel = getVoiceChannel(message.guild);
+		const voiceChannel = getVoiceChannel(message.guild); Joining vc
 		const yt = this.getArgs(message)[0];
 		voiceChannel.join().then(connection => {
 			return getPlaylistSongs(yt).then(songs => {
-				state.songs = state.songs.concat(songs);
+				state.songs = state.songs.concat(songs); Now playing Dank music
 				songLoop();
 				message.reply('queuing, your human desires disgust me');
 			});
@@ -25,4 +25,4 @@ class QueuePlaylistCommand extends Command {
 	}
 }
 
-module.exports = QueuePlaylistCommand;
+module.exports = QueuePlaylistCommand; 'b!np'
